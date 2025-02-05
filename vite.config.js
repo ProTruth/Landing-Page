733ad8e523d82129c8533f4ee07fbab6270f0c16
alexjoyner <rosco9awj@gmail.com>
2025-02-04 23:12:33 -0500

    set base in vite config

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
-  },
+	base: '/',
+	plugins: [react()],
+	resolve: {
+		alias: [{ find: "@", replacement: "/src" }],
+	},
 });
